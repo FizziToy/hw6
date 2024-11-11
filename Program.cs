ef5ab51309cc66d0bda8cfc339c288f59bbaf720
StpKate <fizzitoy@gmail.com>
2024-11-11 09:37:24 +0200

    Program.cs

--- Program.cs
+++ Program.cs
@@ -8,15 +8,11 @@ internal class Program
 {
     private static async Task Main(string[] args)
     {
-        // Вказуємо відносний шлях до файлу JSON
-        string path = @"JsonTask.json"; // Змініть на правильний шлях, якщо потрібно
-
-        // Відкриваємо файл і десеріалізуємо в список книг
+        string path = @"JsonTask.json"; 
         using (FileStream fs = new FileStream(path, FileMode.Open))
         {
             var books = await JsonSerializer.DeserializeAsync<List<Book>>(fs);
 
-            // Виводимо інформацію про кожну книгу
             foreach (var book in books)
             {
                 Console.WriteLine($"Назва: {book.Title}, Видавництво: {book.PublishingHouse.Name}, Адреса: {book.PublishingHouse.Adress}");
